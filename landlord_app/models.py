39616ce8d1c94cc8a67a659598fe0e5bdc60d136
Andrew <andrewdfabrizio@gmail.com>
2020-09-22 23:25:17 -0400

    added tenant start/ends. Made foreignkeys null-able. migrated.

--- landlord_app/models.py
+++ landlord_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from datetime import date
+
+# TO-DO: Add-in image fields
 
 class Users(models.Model):
     username = models.EmailField(max_length=254)
@@ -13,15 +16,21 @@ class Users(models.Model):
     zipcode = models.PositiveIntegerField()
     units = models.ForeignKey(
         'Units',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
     reg_ovrhd_expenses = models.ForeignKey(
         'Reg_Ovrhd_Expenses',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
     ovrhd_expense_pmts = models.ForeignKey(
         'Ovrhd_Expense_Pmts',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
 
 
@@ -37,19 +46,27 @@ class Units(models.Model):
     bathrooms = models.PositiveIntegerField()
     tenants = models.ForeignKey(
         'Tenants',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
     reg_unit_expenses = models.ForeignKey(
         'Reg_Unit_Expenses',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
     unit_expense_pmts = models.ForeignKey(
         'Unit_Expense_Pmts',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
     oth_unit_income = models.ForeignKey(
         'Oth_Unit_Income',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
 
 class Reg_Ovrhd_Expenses(models.Model):
@@ -82,34 +99,50 @@ class Tenants(models.Model):
     tenant1_last = models.CharField(max_length=40)
     tenant1_ssn = models.PositiveIntegerField()
     tenant1_email = models.EmailField(max_length=254)
+    tenant1_start = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
+    tenant1_end = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
     tenant2_first = models.CharField(max_length=40)
     tenant2_last = models.CharField(max_length=40)
     tenant2_ssn = models.PositiveIntegerField()
     tenant2_email = models.EmailField(max_length=254)
+    tenant2_start = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
+    tenant2_end = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
     tenant3_first = models.CharField(max_length=40)
     tenant3_last = models.CharField(max_length=40)
     tenant3_ssn = models.PositiveIntegerField()
     tenant3_email = models.EmailField(max_length=254)
+    tenant3_start = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
+    tenant3_end = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
     tenant4_first = models.CharField(max_length=40)
     tenant4_last = models.CharField(max_length=40)
     tenant4_ssn = models.PositiveIntegerField()
     tenant4_email = models.EmailField(max_length=254)
+    tenant4_start = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
+    tenant4_end = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
     pets = models.JSONField(encoder=None, decoder=None)
     leases = models.ForeignKey(
         'Leases',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
     oth_docs = models.ForeignKey(
         'Oth_Docs',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
     tenant_payments = models.ForeignKey(
         'Tenant_Payments',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
     evict_notices = models.ForeignKey(
         'Evict_Notices',
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
 
 class Reg_Unit_Expenses(models.Model):
