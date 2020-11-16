fff735e51c1d932db1731babe49dff2563e2adfd
Andrew <andrewdfabrizio@gmail.com>
2020-11-16 16:04:53 -0500

    Trimmed scope of models.py

--- landlord_app/models.py
+++ landlord_app/models.py
@@ -3,7 +3,7 @@ from datetime import date
 
 # TO-DO: Add-in image fields
 
-class Users(models.Model):
+class User(models.Model):
     username = models.EmailField(max_length=254)
     password = models.CharField(max_length=40)
     first_name = models.CharField(max_length=40)
@@ -20,22 +20,9 @@ class Users(models.Model):
         blank=True,
         null=True
     )
-    reg_ovrhd_expenses = models.ForeignKey(
-        'Reg_Ovrhd_Expenses',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
-    )
-    ovrhd_expense_pmts = models.ForeignKey(
-        'Ovrhd_Expense_Pmts',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
-    )
 
 
-
-class Units(models.Model):
+class Unit(models.Model):
     nickname = models.CharField(max_length=40)
     address_line1 = models.CharField(max_length=120)
     address_line2 = models.CharField(max_length=120)
@@ -50,84 +37,18 @@ class Units(models.Model):
         blank=True,
         null=True
     )
-    reg_unit_expenses = models.ForeignKey(
-        'Reg_Unit_Expenses',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
-    )
-    unit_expense_pmts = models.ForeignKey(
-        'Unit_Expense_Pmts',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
-    )
-    oth_unit_income = models.ForeignKey(
-        'Oth_Unit_Income',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
-    )
 
-class Reg_Ovrhd_Expenses(models.Model):
-    other_name = models.CharField(max_length=20)
-    other_amount = models.DecimalField(max_digits=9, decimal_places=2)
 
-class Ovrhd_Expense_Pmts(models.Model):
-    payer_first = models.CharField(max_length=40)
-    payer_last = models.CharField(max_length=40)
-    payer_company = models.CharField(max_length=80)
-    payer_bizind = models.BooleanField()
-    payer_email = models.EmailField(max_length=254)
-    pay_method = models.CharField(max_length=20)
-    amount = models.DecimalField(max_digits=9, decimal_places=2)
-    date_paid = models.DateField(auto_now=False, auto_now_add=False)
-    reason = models.CharField(max_length=40)
-    rtn_number = models.PositiveIntegerField()
-    acct_number = models.PositiveIntegerField()
-    acct_type = models.CharField(max_length=1)
-    check_num = models.PositiveIntegerField()
-#    check_img = ImageField()
-    cc_num = models.PositiveIntegerField()
-    cc_exp_month = models.PositiveIntegerField()
-    cc_exp_year = models.PositiveIntegerField()
-    cc_vercode = models.PositiveIntegerField()
-
-class Tenants(models.Model):
+class Tenant(models.Model):
     tenant_number = models.PositiveIntegerField()
-    tenant1_first = models.CharField(max_length=40)
-    tenant1_last = models.CharField(max_length=40)
-    tenant1_ssn = models.PositiveIntegerField()
-    tenant1_email = models.EmailField(max_length=254)
-    tenant1_start = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
-    tenant1_end = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
-    tenant2_first = models.CharField(max_length=40)
-    tenant2_last = models.CharField(max_length=40)
-    tenant2_ssn = models.PositiveIntegerField()
-    tenant2_email = models.EmailField(max_length=254)
-    tenant2_start = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
-    tenant2_end = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
-    tenant3_first = models.CharField(max_length=40)
-    tenant3_last = models.CharField(max_length=40)
-    tenant3_ssn = models.PositiveIntegerField()
-    tenant3_email = models.EmailField(max_length=254)
-    tenant3_start = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
-    tenant3_end = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
-    tenant4_first = models.CharField(max_length=40)
-    tenant4_last = models.CharField(max_length=40)
-    tenant4_ssn = models.PositiveIntegerField()
-    tenant4_email = models.EmailField(max_length=254)
-    tenant4_start = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
-    tenant4_end = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
-    pets = models.JSONField(encoder=None, decoder=None)
+    tenant_first = models.CharField(max_length=40)
+    tenant_last = models.CharField(max_length=40)
+    tenant_ssn = models.PositiveIntegerField()
+    tenant_email = models.EmailField(max_length=254)
+    tenant_start = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
+    tenant_end = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
     leases = models.ForeignKey(
         'Leases',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
-    )
-    oth_docs = models.ForeignKey(
-        'Oth_Docs',
         on_delete=models.SET_NULL,
         blank=True,
         null=True
@@ -145,58 +66,8 @@ class Tenants(models.Model):
         null=True
     )
 
-class Reg_Unit_Expenses(models.Model):
-    mortgage = models.DecimalField(max_digits=9, decimal_places=2)
-    insurance = models.DecimalField(max_digits=9, decimal_places=2)
-    taxes = models.DecimalField(max_digits=9, decimal_places=2)
-    hoa = models.DecimalField(max_digits=9, decimal_places=2)
-    electric = models.DecimalField(max_digits=9, decimal_places=2)
-    water = models.DecimalField(max_digits=9, decimal_places=2)
-    garbage = models.DecimalField(max_digits=9, decimal_places=2)
-    other_name = models.CharField(max_length=20)
-    other_amount = models.DecimalField(max_digits=9, decimal_places=2)
 
-class Unit_Expense_Pmts(models.Model):
-    payer_first = models.CharField(max_length=40)
-    payer_last = models.CharField(max_length=40)
-    payer_company = models.CharField(max_length=80)
-    payer_bizind = models.BooleanField()
-    payer_email = models.EmailField(max_length=254)
-    pay_method = models.CharField(max_length=20)
-    amount = models.DecimalField(max_digits=9, decimal_places=2)
-    date_paid = models.DateField(auto_now=False, auto_now_add=False)
-    reason = models.CharField(max_length=40)
-    rtn_number = models.PositiveIntegerField()
-    acct_number = models.PositiveIntegerField()
-    acct_type = models.CharField(max_length=1)
-    check_num = models.PositiveIntegerField()
-#    check_img = ImageField()
-    cc_num = models.PositiveIntegerField()
-    cc_exp_month = models.PositiveIntegerField()
-    cc_exp_year = models.PositiveIntegerField()
-    cc_vercode = models.PositiveIntegerField()
-
-class Oth_Unit_Income(models.Model):
-    payer_first = models.CharField(max_length=40)
-    payer_last = models.CharField(max_length=40)
-    payer_company = models.CharField(max_length=80)
-    payer_bizind = models.BooleanField()
-    payer_email = models.EmailField(max_length=254)
-    pay_method = models.CharField(max_length=20)
-    amount = models.DecimalField(max_digits=9, decimal_places=2)
-    date_paid = models.DateField(auto_now=False, auto_now_add=False)
-    reason = models.CharField(max_length=40)
-    rtn_number = models.PositiveIntegerField()
-    acct_number = models.PositiveIntegerField()
-    acct_type = models.CharField(max_length=1)
-    check_num = models.PositiveIntegerField()
-#    check_img = ImageField()
-    cc_num = models.PositiveIntegerField()
-    cc_exp_month = models.PositiveIntegerField()
-    cc_exp_year = models.PositiveIntegerField()
-    cc_vercode = models.PositiveIntegerField()
-
-class Leases(models.Model):
+class Lease(models.Model):
     rent_amount = models.DecimalField(max_digits=9, decimal_places=2)
     secdep_amount = models.DecimalField(max_digits=9, decimal_places=2)
     start_date = models.DateField(auto_now=False, auto_now_add=False)
@@ -212,12 +83,8 @@ class Leases(models.Model):
     othfee_amt = models.DecimalField(max_digits=9, decimal_places=2)
     rollover = models.CharField(max_length=1)
 
-class Oth_Docs(models.Model):
-    doc_type = models.CharField(max_length=40)
-    date_signed = models.DateField(auto_now=False, auto_now_add=False)
-#    doc_img = ImageField()
 
-class Tenant_Payments(models.Model):
+class Tenant_Payment(models.Model):
     payer_first = models.CharField(max_length=40)
     payer_last = models.CharField(max_length=40)
     payer_company = models.CharField(max_length=80)
@@ -237,7 +104,7 @@ class Tenant_Payments(models.Model):
     cc_exp_year = models.PositiveIntegerField()
     cc_vercode = models.PositiveIntegerField()
 
-class Evict_Notices(models.Model):
+class Evict_Notice(models.Model):
     notice_type = models.CharField(max_length=40)
     date_sent = models.DateField(auto_now=False, auto_now_add=False)
     delivery_method = models.CharField(max_length=40)
