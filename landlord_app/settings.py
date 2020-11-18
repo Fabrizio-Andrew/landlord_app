f8c379d7d499c22a705ce724d7d948d26327459f
Andrew <andrewdfabrizio@gmail.com>
2020-11-18 16:31:09 -0500

    Changed to SQLite

--- landlord_app/settings.py
+++ landlord_app/settings.py
@@ -78,15 +78,12 @@ WSGI_APPLICATION = 'landlord_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'Andrew',
-        'PASSWORD': 'RwSCcX6xMQHudHU1XM9z',
-        'HOST': 'landlord-db.cv13zxmqpszo.us-west-2.rds.amazonaws.com',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+AUTH_USER_MODEL="landlord_app.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
