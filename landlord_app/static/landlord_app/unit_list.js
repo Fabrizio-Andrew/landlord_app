4591f59b0c5b77f63d0c2003fd57fb2958d979fd
Andrew <andrewdfabrizio@gmail.com>
2020-12-04 23:50:32 -0500

    Added Edit Unit form front end

--- landlord_app/static/landlord_app/unit_list.js
+++ landlord_app/static/landlord_app/unit_list.js
@@ -4,8 +4,11 @@ class Unit extends React.Component {
         super(props);
  
         this.state = {
-            unit: null,
+            unit: '',
+            showcontent: true,
+            editclicked: false
         };
+        this.edit_click = this.edit_click.bind(this);
     }
 
     render() {
@@ -27,49 +30,49 @@ class Unit extends React.Component {
         }
 
         // Create a uniqe ID for each card
-        var uniqueid = `card-number-${this.props.unit["id"]}`;
+        var uniqueid = `card-content-${this.props.unit["id"]}`;
 
         return (
-            <div key={this.props.unit["id"]} className="card mb-3" id={uniqueid}>
-                <div className="row no-gutters">
-                    <div className="col-md-3" style={{backgroundColor: "grey"}}>
-                        <svg width="8em" height="8em" viewBox="0 0 16 16" className="bi bi-house-door" fill="rgb(1, 143, 1)" xmlns="http://www.w3.org/2000/svg">
-                            <path fillRule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z"/>
-                            <path fillRule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
-                        </svg>
-                    </div>
-                    <div className="col-md-7">
-                        <div className="card-body">
-                            <h5 className="card-title">{this.props.unit["nickname"]}</h5>
-                            <p className="card-text">{this.props.unit["address_line1"]}, {this.props.unit["address_line2"]} {this.props.unit["city"]}, {this.props.unit["state"]} {this.props.unit["zipcode"]}</p>
-                            {tenant}
-                            {leaseline}
+            <div key={this.props.unit["id"]} className="card mb-3">
+                {this.state.showcontent && 
+                    <div className="row no-gutters" id={uniqueid}>
+                        <div className="col-md-3" style={{backgroundColor: "grey"}}>
+                            <svg width="8em" height="8em" viewBox="0 0 16 16" className="bi bi-house-door" fill="rgb(1, 143, 1)" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z"/>
+                                <path fillRule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                            </svg>
+                        </div>
+                        <div className="col-md-7">
+                            <div className="card-body">
+                                <h5 className="card-title">{this.props.unit["nickname"]}</h5>
+                                <p className="card-text">{this.props.unit["address_line1"]}, {this.props.unit["address_line2"]} {this.props.unit["city"]}, {this.props.unit["state"]} {this.props.unit["zipcode"]}</p>
+                                {tenant}
+                                {leaseline}
+                            </div>
+                        </div>
+                        <div className="col-md-2" style={{backgroundColor: "grey"}}>
+                            <div className="row no-gutters">
+                                <button type="button" class="btn btn-primary" onClick={this.edit_click}>Edit</button>
+                                
+                            </div>
+                            {remtenant}
                         </div>
                     </div>
-                    <div className="col-md-2" style={{backgroundColor: "grey"}}>
-                        <div className="row no-gutters">
-                            <button onClick ={this.edit_unit} type="button" class="btn btn-primary">Edit</button>
-                        </div>
-                        {remtenant}
-                    </div>
-                </div>
+                }
+                {this.state.editclicked && <EditUnitForm />}
             </div>
         );
     }
 
-    edit_unit() {
-
-        console.log(this.state.unit["id"]);
-        // Get the current Unit Card
-        var unitcard = document.querySelector(`#card-number-${this.state.unit["id"]}`);
-    
-        // Clear the Unit Card
-        unitcard.style.display = 'none';
-    
-        // Display the Edit Unit Form
-        ReactDOM.render(React.createElement(<EditUnitForm unit={this.state.unit}/>), unitcard);
+    edit_click() {
+        
+        // Hide Unit Content and show the edit form
+        this.setState({
+            showcontent: false,
+            editclicked: true
+        });
     }
-}
+};
 
 
 
@@ -98,13 +101,13 @@ class EditUnitForm extends React.Component {
                             <div className="input-group-prepend">
                                 <span className="input-group-text" id="inputGroup-sizing-sm">Nickname</span>
                             </div>
-                            <input type="text" className="form-control" aria-label="Nickname" aria-describedby="inputGroup-sizing-sm">{this.props.unit["nickname"]}</input>
+                            <input type="text" className="form-control" aria-label="Nickname" aria-describedby="inputGroup-sizing-sm"></input>
                         </div>
                         <div className="input-group input-group-sm mb-3">
                             <div className="input-group-prepend">
                                 <span className="input-group-text" id="inputGroup-sizing-sm">Street Address Line 1</span>
                             </div>
-                            <input type="text" className="form-control" aria-label="Street Address Line 1" aria-describedby="inputGroup-sizing-sm">{this.props.unit["address_line1"]}</input>
+                            <input type="text" className="form-control" aria-label="Street Address Line 1" aria-describedby="inputGroup-sizing-sm"></input>
                         </div>
                         <div className="input-group input-group-sm mb-3">
                             <div className="input-group-prepend">
@@ -191,7 +194,7 @@ class EditUnitForm extends React.Component {
         );
     }
     
-}
+};
 
 class AddUnitButton extends React.Component {
 
